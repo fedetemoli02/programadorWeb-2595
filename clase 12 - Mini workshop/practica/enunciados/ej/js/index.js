3ae24a1c20bd1338b9089f1e027f4562d3658d1c
Adrián Ferré <adrian_c4@hotmail.com>
2017-11-25 18:37:38 -0300

    Add classes

--- clase 12 - Mini workshop/practica/enunciados/ej/js/index.js
+++ clase 12 - Mini workshop/practica/enunciados/ej/js/index.js
@@ -1,3 +1,12 @@
-console.log('Clase 12 - Mini Workshop')
+$(document).ready(function () {
+  console.log('Clase 12 - Mini Workshop')
 
-$(document).ready(function () {})
+  list.init()
+
+  var addFirstNameButton = $('#addFirstNameButton').click(function () {
+    var firstNameInput = $('#addFirstName')
+    var value = firstNameInput.val()
+
+    list.addName(value)
+  })
+})
