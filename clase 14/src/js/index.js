51f673e74f34a63f1665bad394b7caca622122ca
Adrián Ferré <adrian_c4@hotmail.com>
2017-12-04 10:56:29 -0300

    Add date

--- clase 14/src/js/index.js
+++ clase 14/src/js/index.js
@@ -9,22 +9,30 @@ console.log('Clase 14')
 //   })
 // })
 
-$(document).ready(function () {
-  $('#date').blur(function () {
-    var date = $('#date').val()
-    var today = moment()
-    var actualMonth = today.month()
-    var actualDay = today.date()
+// $(document).ready(function () {
+//   $('#date').blur(function () {
+//     var date = $('#date').val()
+//     var today = moment()
+//     var actualMonth = today.month()
+//     var actualDay = today.date()
 
-    var formatedDate = moment(date)
-    var formatedMonth = formatedDate.month()
-    var formatedDay = formatedDate.date()
+//     var formatedDate = moment(date)
+//     var formatedMonth = formatedDate.month()
+//     var formatedDay = formatedDate.date()
 
-    // var diff = moment(today).diff(formatedDate)
-    if (actualDay === formatedDay && actualMonth === formatedMonth) {
-      console.log('Es tu cumpleaños')
-    } else {
-      console.log('Día cualquiera')
-    }
-  })
-})
+//     // var diff = moment(today).diff(formatedDate)
+//     if (actualDay === formatedDay && actualMonth === formatedMonth) {
+//       console.log('Es tu cumpleaños')
+//     } else {
+//       console.log('Día cualquiera')
+//     }
+//   })
+// })
+
+var today = moment()
+
+var christmas = moment('20171225')
+
+var diff = christmas.diff(today, 'days')
+
+console.log(diff)
