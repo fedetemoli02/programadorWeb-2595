fb1def2d41b04513d42519d137a6d8024a623812
Adrián Ferré <adrian_c4@hotmail.com>
2017-12-16 11:24:18 -0300

    Add clases

--- clase 15/src/js/index.js
+++ clase 15/src/js/index.js
@@ -1,1 +1,47 @@
 console.log('Clase 15')
+function ajaxCall (url, cbk) {
+  $.ajax({
+    url: url,
+    method: 'GET'
+  })
+    .done(function (response) {
+      cbk(null, response)
+    })
+    .fail(function (error) {
+      cbk(error)
+    })
+}
+
+// var resultTotal = []
+// var counter = 0
+
+// var callback = function (error, response) {
+//   if (error) {
+//     console.log('Error', error)
+//   } else {
+//     counter = counter + response.results.length
+//     console.log('Encontrado ' + counter + ' de ' + response.count)
+//     resultTotal = resultTotal.concat(response.results)
+//     if (response.next) {
+//       callUrl = response.next
+//       ajaxCall(callUrl, callback)
+//     } else {
+//       console.log('Termino de buscar')
+//       console.log(resultTotal)
+//     }
+//   }
+// }
+
+// var callUrl = 'https://swapi.co/api/starships'
+// console.log('Empiezo a buscar')
+// ajaxCall(callUrl, callback)
+
+var text = prompt('Ingrese un texto')
+
+ajaxCall('https://swapi.co/api/starships/?search=' + text, function (error, result) {
+  if (error) {
+    console.log('Error', error)
+  } else {
+    console.log(result)
+  }
+})
